3109d08feed0669e8a54d49a56f06f95d16a4b10
KeenanHoffman <keenan.m.hoffman@gmail.com>
2015-12-30 10:49:19 -0600

    added support for flash messages and example route /flash

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {title: 'Pong'});
+});
+
+router.get('/flash', function(req, res, next) {
+  res.render('flash', {title: 'Pong'});
+});
+
+router.post('/flash', function(req, res, next) {
+  var userSubmission = req.body;
+  if (!userSubmission.password) {
+    req.flash('info', 'Please enter a password');
+  } else if (userSubmission.password.length < 7) {
+    req.flash('info', 'Passwords must be 8 or more characters long');
+  } else {
+    req.flash('info', 'Valid password');
+  }
+  res.render('flash', {
+    title: 'Pong',
+    message: req.flash('info')
+  });
 });
 
 module.exports = router;
