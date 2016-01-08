45d95177ff7244b7f184a4335d26b74d54795236
David Bayless <David.C.Bayless15@gmail.com>
2016-01-08 09:15:01 -0700

    added error messages to incomplete new user form

--- routes/users.js
+++ routes/users.js
@@ -78,13 +78,50 @@ router.get('/signup', function(req, res, next) {
 router.post('/signup', function(req, res, next) {
   console.log(req.body);
   var passerr = '';
+  var passGo = false;
+  var usernameerr = '';
+  var usernameGo = false;
+  var firstnameerr = '';
+  var firstnameGo = false;
+  var lastnameerr = '';
+  var lastnameGo = false;
   if(req.body.firstname && req.body.lastname && req.body.username && req.body.email && req.body.password && req.body.passwordTwo) {
     if (req.body.password === req.body.passwordTwo) {
-      res.redirect('/users/profile');
+      passGo = true;
     } else {
       passerr = 'Passwords do not match';
     }
-    if (account.validUsername(req.body.username))
+
+    console.log(req.body.username.length);
+    if (req.body.username.length > 3 && req.body.username.length < 25) {
+      usernameGo = true;
+    } else if (req.body.username.length > 25) {
+      usernameerr = 'Username is too long';
+    } else if (req.body.username.length <= 3) {
+      usernameerr = 'Username is too short';
+    }
+
+    if (account.validName(req.body.firstname)) {
+      firstnameGo = true;
+    } else if (req.body.firstname.length > 17){
+      firstnameerr = 'First name is too long';
+    } else if (/[\*,&^%$#@!)(-+\s|}{\\`~/?><.';:"0-9]/g.test(req.body.firstname)) {
+      firstnameerr = 'Name can only be letters';
+    }
+
+    if (account.validName(req.body.lastname)) {
+      lastnameGo = true;
+    } else if (req.body.lastname.length > 17){
+      lastnameerr = 'Last name is too long';
+    } else if (/[\*,&^%$#@!)(-+\s|}{\\`~/?><.';:"0-9]/g.test(req.body.lastname)) {
+      lastnameerr = 'Name can only be letters';
+    }
+
+    if (passGo && usernameGo && firstnameGo && lastnameGo) {
+      res.redirect('profile');
+    } else {
+      res.render('newUser', {firstname: req.body.firstname, lastname: req.body.lastname, username: req.body.username, email: req.body.email, passerr: passerr, usernameerr: usernameerr, firstnameerr: firstnameerr, lastnameerr: lastnameerr});
+    }
   } else {
     res.render('newUser', {firstname: req.body.firstname, lastname: req.body.lastname, username: req.body.username, email: req.body.email});
   }
